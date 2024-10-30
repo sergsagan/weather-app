12a04e1cc4cd58f3fcaf2ab493a6f7af91121435
sergsagan <sergey.martynuk@gmail.com>
2024-10-30 12:11:04 +0100

    api request

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,51 @@
-<script setup></script>
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const mapboxAPIKey =
+  'pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q';
+const searchQuery = ref('');
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(searchQuery.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== '') {
+      const result = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`,
+      );
+      mapboxSearchResults.value = result.data.features;
+      console.log(mapboxSearchResults.value);
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+</script>
 
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="relative mb-8 pt-4">
+      <input
+        class="w-full border-b bg-transparent px-1 py-2 focus:border-weather-secondary focus:shadow-[0px_1px_0_0_#004E71] focus:outline-none"
+        type="text"
+        placeholder="Search for a city or state"
+        v-model="searchQuery"
+        @input="getSearchResults"
+      />
+      <ul
+        v-if="mapboxSearchResults"
+        class="absolute top-[66px] w-full bg-weather-secondary px-1 py-2 text-white shadow-md"
+      >
+        <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="cursor-pointer py-2"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
